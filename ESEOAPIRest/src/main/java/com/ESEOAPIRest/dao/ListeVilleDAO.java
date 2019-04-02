9496ffc5b28fe8ae47cf05c7f594c908f4084331
bourearo <romain.boureau@reseau.eseo.fr>
2019-04-02 10:48:24 +0200

    Première version de la dto Ville et de la DAO ListeVilleDAO

--- ESEOAPIRest/src/main/java/com/ESEOAPIRest/dao/ListeVilleDAO.java
+++ ESEOAPIRest/src/main/java/com/ESEOAPIRest/dao/ListeVilleDAO.java
@@ -1,5 +1,211 @@
 package com.ESEOAPIRest.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import com.ESEOAPIRest.dto.Ville;
+
+
+
 public class ListeVilleDAO {
+private String tableName = "ville_france";
 	
+	public ListeVilleDAO() {
+		super();
+	}
+	
+	public boolean creationVille(Ville ville) {
+		
+		
+		boolean init = false;
+		try {
+			Connection conn = Connexion.getInstance();
+			
+			String query = "INSERT INTO " + tableName ;
+			query += "(Code_commune_INSEE, Nom_commune, Code_postal, Libelle_acheminement, Ligne_5, Latitude, Longitude)";
+			query += " VALUES(?,?,?,?,?,?,?)";
+			
+			PreparedStatement prepare = conn.prepareStatement(query,ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+			
+			prepare.setString(1, ville.getCodeCommuneINSEE());
+			prepare.setString(2, ville.getNomCommune());
+			prepare.setString(3, ville.getCodePostal());
+			prepare.setString(4, ville.getLibelleAcheminement());
+			prepare.setString(5, ville.getLigne5());
+			prepare.setString(6, ville.getLatitude());
+			prepare.setString(7, ville.getLongitude());
+			
+			
+			prepare.executeUpdate();
+			init = true;
+			
+			
+			
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+		
+		return init;
+	}
+	
+	public Ville [] listeVilles() {
+	      Ville [] villes = null;
+		
+		try {
+			Connection conn = Connexion.getInstance();
+			String query = "SELECT * FROM "+tableName;
+
+			
+			PreparedStatement prepare = conn.prepareStatement(query,ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+			
+			ResultSet res = prepare.executeQuery();
+			res.last();
+			int size = res.getRow();
+			res.beforeFirst();
+			
+			villes = new Ville [size];
+			
+			int i = 0;
+			while (res.next()) {
+				Ville ville = new Ville();
+				
+				ville.setCodeCommuneINSEE(res.getString("Code_commune_INSEE"));
+				ville.setNomCommune(res.getString("Nom_commune"));
+				ville.setCodePostal(res.getString("Code_postal"));
+				ville.setLibelleAcheminement(res.getString("Libelle_acheminement"));
+				ville.setLigne5(res.getString("Ligne_5"));
+				ville.setLatitude(res.getString("Latitude"));
+				ville.setLongitude(res.getString("Longitude"));
+				
+				villes[i] = ville;
+				i++;
+			}
+			
+			res.close();
+			prepare.close();
+			
+			
+			
+		}catch (Exception e) {
+		      e.printStackTrace();
+
+
+	    }
+		return villes;
+	}
+	public Ville [] listeVillesAvecFiltre(int filtre, String value) {
+	      Ville [] villes = null;
+	      
+	      String filtreRequete;
+	      switch(filtre)
+	    	        {
+	                case 1:
+	                	filtreRequete="Code_commune_INSEE";
+	                break;
+	                case 2:
+	                	filtreRequete="Nom_commune";
+	                break;
+	                case 3:
+	                	filtreRequete="Code_postal";
+	                break;
+	                case 4:
+	                	filtreRequete="Libelle_acheminement";
+	                break;
+	                case 5:
+	                	filtreRequete="Ligne_5";
+	                break;
+	                case 6:
+	                	filtreRequete="Latitude";
+	                break;
+	                case 7:
+	                	filtreRequete="Longitude";
+	                break;
+	                default:
+	                	filtreRequete="";
+	                break;
+	            }
+		
+		try {
+			Connection conn = Connexion.getInstance();
+			String query;
+			if(filtreRequete=="") {
+				query = "SELECT * FROM "+tableName;
+			}else {
+				query = "SELECT * FROM "+tableName+" WHERE "+filtreRequete+" = "+value;
+			}
+			
+
+			
+			PreparedStatement prepare = conn.prepareStatement(query,ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+			
+			ResultSet res = prepare.executeQuery();
+			res.last();
+			int size = res.getRow();
+			res.beforeFirst();
+			
+			villes = new Ville [size];
+			
+			int i = 0;
+			while (res.next()) {
+				Ville ville = new Ville();
+				
+				ville.setCodeCommuneINSEE(res.getString("Code_commune_INSEE"));
+				ville.setNomCommune(res.getString("Nom_commune"));
+				ville.setCodePostal(res.getString("Code_postal"));
+				ville.setLibelleAcheminement(res.getString("Libelle_acheminement"));
+				ville.setLigne5(res.getString("Ligne_5"));
+				ville.setLatitude(res.getString("Latitude"));
+				ville.setLongitude(res.getString("Longitude"));
+				
+				villes[i] = ville;
+				i++;
+			}
+			
+			res.close();
+			prepare.close();
+			
+			
+			
+		}catch (Exception e) {
+		      e.printStackTrace();
+
+
+	    }
+		return villes;
+	}
+	
+	public boolean suppressionVille(Ville ville) {
+		
+		
+		boolean init = false;
+		try {
+			Connection conn = Connexion.getInstance();
+			
+			String query = "DELETE FROM " + tableName ;
+			query += " WHERE Code_commune_INSEE=? AND Nom_commune=? AND Code_postal=? AND Libelle_acheminement=? AND Ligne_5=? AND Latitude=? AND Longitude=?";
+			
+			
+			PreparedStatement prepare = conn.prepareStatement(query,ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+			
+			prepare.setString(1, ville.getCodeCommuneINSEE());
+			prepare.setString(2, ville.getNomCommune());
+			prepare.setString(3, ville.getCodePostal());
+			prepare.setString(4, ville.getLibelleAcheminement());
+			prepare.setString(5, ville.getLigne5());
+			prepare.setString(6, ville.getLatitude());
+			prepare.setString(7, ville.getLongitude());
+			
+			
+			prepare.executeUpdate();
+			init = true;
+			
+			
+			
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+		
+		return init;
+	}
 }
--- ESEOAPIRest/src/main/java/com/ESEOAPIRest/dto/Ville.java
+++ ESEOAPIRest/src/main/java/com/ESEOAPIRest/dto/Ville.java
@@ -1,5 +1,73 @@
 package com.ESEOAPIRest.dto;
 
 public class Ville {
+	private String codeCommuneINSEE;
+	private String nomCommune;
+	private String codePostal;
+	private String libelleAcheminement;
+	private String ligne5;
+	private String latitude;
+	private String longitude;
+	
+	
+	
+	public Ville() {
+		super();
+	}
+	
+	public Ville(String codeCommuneINSEE, String nomCommune, String codePostal, String libelleAcheminement, String ligne5,
+			String latitude, String longitude) {
+		super();
+		this.codeCommuneINSEE = codeCommuneINSEE;
+		this.nomCommune = nomCommune;
+		this.codePostal = codePostal;
+		this.libelleAcheminement = libelleAcheminement;
+		this.ligne5 = ligne5;
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
 
+	public String getCodeCommuneINSEE() {
+		return codeCommuneINSEE;
+	}
+	public void setCodeCommuneINSEE(String codeCommuneINSEE) {
+		this.codeCommuneINSEE = codeCommuneINSEE;
+	}
+	public String getNomCommune() {
+		return nomCommune;
+	}
+	public void setNomCommune(String nomCommune) {
+		this.nomCommune = nomCommune;
+	}
+	public String getCodePostal() {
+		return codePostal;
+	}
+	public void setCodePostal(String codePostal) {
+		this.codePostal = codePostal;
+	}
+	public String getLibelleAcheminement() {
+		return libelleAcheminement;
+	}
+	public void setLibelleAcheminement(String libelleAcheminement) {
+		this.libelleAcheminement = libelleAcheminement;
+	}
+	public String getLigne5() {
+		return ligne5;
+	}
+	public void setLigne5(String ligne5) {
+		this.ligne5 = ligne5;
+	}
+	public String getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+	public String getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
+	
 }
